d54919eabd7a3b13a704ebe9ecae94f4f88e0145
Ingy döt Net <ingy@ingy.net>
2015-04-27 16:53:24 -0400

    Perl 6 Emitter

---
+++ lib/Lingy/Emitter/Perl6.pm
@@ -0,0 +1,36 @@
+package Lingy::Emitter::Perl6;
+use Lingy::Base;
+extends 'Lingy::Emitter';
+
+sub emit_class_head {
+    my ($self, $name, $body) = @_;
+    <<"...";
+class $name;
+
+...
+}
+
+sub emit_method_head {
+    my ($self, $name, $body) = @_;
+    my $arg_list = join ', ', map {
+        my ($var, $type) = each %$_;
+        "\$$var";
+    } @{$body->args};
+    my $out = <<"...";
+method $name($arg_list) {
+...
+}
+
+sub emit_method_foot {
+    my ($self, $name, $body) = @_;
+    "};\n\n";
+}
+
+# XXX This needs to be implemented (just faked it):
+sub emit_statement {
+    my ($self, $statement) = @_;
+    qq{  say 'Hello ' ~ \$name;\n};
+}
+
+1;
+
