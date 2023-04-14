8b33de8dffa6509e94ccd8b463122b293d2d7c8c
Ingy döt Net <ingy@ingy.net>
2023-04-13 17:52:53 -0700

    Ruby 0.0.1

---
+++ ruby/ChangeLog.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.0.1] - 2023-04-13
+
+- Initial release
---
+++ ruby/lib/yamlscript.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lingy/version"
+
+module lingy
+  class Error < StandardError; end
+  # Your code goes here...
+end
