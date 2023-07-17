b4296b7725a82d2567d33d474d8d053611454cbf
Peter Strömberg <pez@pezius.com>
2023-07-18 00:16:47 +0200

    WIP receive/send

--- perl/lib/Lingy/nREPL.pm
+++ perl/lib/Lingy/nREPL.pm
@@ -2,6 +2,8 @@ use strict; use warnings;
 package Lingy::nREPL;
 
 use IO::Socket::INET;
+use Bencode;
+use Data::Dumper;
 
 use XXX;
 
@@ -34,8 +36,41 @@ sub start {
 
     print "Starting nrepl://127.0.0.1:$port\n";
 
+    CONN:
     while (my $conn = $self->{socket}->accept) {
-        close($conn);
+        print "Accepted a new connection\n";
+        my $buffer = '';
+        while (my $bytes_read = sysread($conn, $buffer, 65535)) {
+            print "Read $bytes_read bytes\n";
+            print "Buffer: $buffer\n";
+            print "Decoding...\n";
+            my $received = Bencode::bdecode($buffer, 1);
+            $buffer = '';
+            print ref($received) . "\n";
+            print Dumper($received);
+            if ($received->{'op'} eq 'eval') {
+                my $send = {
+                    ':session' => $received->{'session'},
+                    ':id' => "$received->{'id'}",
+                    ':value' => 'foo'
+                };
+                my $encoded = Bencode::bencode($send);
+                print $conn Bencode::bencode($send);
+                my $done = {
+                    ':session' => $received->{'session'},
+                    ':id' => "$received->{'id'}",
+                    ':status' => 'done',
+                };
+                print $conn Bencode::bencode($done);
+            } elsif ($received->{'op'} eq 'clone') {
+                print "Cloning...\n";
+            } elsif ($received->{'op'} eq 'close') {
+                close($conn);
+                last CONN;
+            } else {
+                print "Unknown op: " . $received->{'op'} . "\n";
+            }
+        }
     }
 }
 
