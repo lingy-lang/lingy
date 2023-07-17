3a5291e4c191638549741b137d45c7bbd51cb035
Peter Strömberg <pez@pezius.com>
2023-07-18 00:16:47 +0200

    More work on nREPL

--- perl/lib/Lingy/nREPL.pm
+++ perl/lib/Lingy/nREPL.pm
@@ -2,6 +2,8 @@ use strict; use warnings;
 package Lingy::nREPL;
 
 use IO::Socket::INET;
+use Bencode;
+use Data::Dumper;
 
 use XXX;
 
@@ -35,7 +37,52 @@ sub start {
     print "Starting nrepl://127.0.0.1:$port\n";
 
     while (my $conn = $self->{socket}->accept) {
-        close($conn);
+        print "Accepted a new connection\n";
+        my $buffer = '';
+        while (my $bytes_read = sysread($conn, $buffer, 65535)) {
+            print "Read $bytes_read bytes\n";
+            print "Buffer: $buffer\n";
+            print "Decoding...\n";
+            my $received = Bencode::bdecode($buffer, 1);
+            $buffer = '';
+            print ref($received) . "\n";
+            print Dumper($received);
+            if ($received->{'op'} eq 'eval') {
+                my $send = {
+                    'session' => $received->{'session'},
+                    'id' => "$received->{'id'}",
+                    'value' => 'foo'
+                };
+                print $conn Bencode::bencode($send);
+                my $done = {
+                    'session' => $received->{'session'},
+                    'id' => "$received->{'id'}",
+                    'status' => 'done',
+                };
+                print $conn Bencode::bencode($done);
+            } elsif ($received->{'op'} eq 'clone') {
+                my $session = 'a-new-session';
+                print "Cloning... new-session: '$session'\n";
+                my $send = {
+                    'id' => "$received->{'id'}",
+                    'new-session' => $session,
+                    'status' => 'done',
+                };
+                print $conn Bencode::bencode($send);
+            } elsif ($received->{'op'} eq 'describe') {
+                print "Describe...\n";
+                my $send = {
+                    'id' => "$received->{'id'}",
+                    'ops' => {'eval' => {}, 'clone' => {}, 'describe' => {}, 'close' => {}},
+                    'status' => 'done',
+                };
+                print $conn Bencode::bencode($send);
+            } elsif ($received->{'op'} eq 'close') {
+                print "TBD: Close session...\n";
+            } else {
+                print "Unknown op: " . $received->{'op'} . "\n";
+            }
+        }
     }
 }
 
